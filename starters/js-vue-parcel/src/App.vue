4bac22a04a377e6df5586d3b7a9e230e202a54e0
Peter Cosemans <peter.cosemans@gmail.com>
2018-06-22 18:04:25 +0200

    add js-vue-parcel

---
+++ starters/js-vue-parcel/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1 className="App-Title">Hello Parcel x Vuejs</h1>
+    <logo />
+  </div>
+</template>
+
+<script>
+import Logo from './logo.svg';
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>

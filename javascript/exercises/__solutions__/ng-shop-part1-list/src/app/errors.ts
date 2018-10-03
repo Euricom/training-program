4b8d8cbee03519e8dcabece4d2f4e7aae6fdf693
Peter Cosemans <peter.cosemans@gmail.com>
2018-10-03 12:16:50 +0200

    cleanup

--- javascript/exercises/__solutions__/ng-shop-part1-list/src/app/errors.ts
+++ javascript/exercises/__solutions__/ng-shop-part1-list/src/app/errors.ts
@@ -1,43 +1,4 @@
-class ExtendableError extends Error {
-  constructor(...params) {
-    super(...params);
-    const message =
-      params.length > 0 && typeof params[0] === 'string' ? params[0] : '';
-
-    // Replace Error with ClassName of the constructor, if it has not been overwritten already
-    if (this.name === undefined || this.name === 'Error') {
-      Object.defineProperty(this, 'name', {
-        configurable: true,
-        enumerable: false,
-        value: this.constructor.name,
-        writable: true,
-      });
-    }
-
-    Object.defineProperty(this, 'message', {
-      configurable: true,
-      enumerable: false,
-      value: message,
-      writable: true,
-    });
-
-    Object.defineProperty(this, 'stack', {
-      configurable: true,
-      enumerable: false,
-      value: '',
-      writable: true,
-    });
-
-    // Maintains proper stack trace for where our error was thrown (only available on V8)
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, this.constructor);
-    } else if (this.stack === '') {
-      this.stack = new Error(message).stack;
-    }
-  }
-}
-
-export class RequestError extends ExtendableError {
+export class RequestError extends Error {
   details: any;
   constructor(
     public status: number,
@@ -61,6 +22,8 @@ export class RequestError extends ExtendableError {
 
     this.message = `Bad response from the server [${status}]`;
     this.details = response.error;
+    this.status = status;
+    this.statusText = statusText;
   }
 }
 
